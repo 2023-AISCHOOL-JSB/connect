e03f1ccbc0be62d5df2e786aede3260f161d85f2
이치연 <leechi2228@gamil.com>
2023-08-12 18:21:46 +0900

    초기화

--- public/js/components/search.js
+++
@@ -1,6 +0,0 @@
-const searchBtn = document.querySelector('.search__button')
-const searchInput = document.querySelector('.search__input')
-
-searchBtn.addEventListener('click', () => {
-    searchInput.classList.toggle('search__show')
-})
